9003b722ab795511264015c56ae6723254a26509
Beqa1G <beqa.gurgenidze.2013@gmail.com>
2023-09-23 19:15:04 +0400

    created user schema

--- backend/src/schema/schema.ts
+++ backend/src/schema/schema.ts
@@ -1,4 +1,5 @@
 import {
+  date,
   integer,
   pgEnum,
   pgTable,
@@ -25,8 +26,27 @@ export const countries = pgTable(
   }
 );
 
+export type Country = typeof countries.$inferSelect;
+
 export const cities = pgTable("cities", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }),
   countryId: integer("country_id").references(() => countries.id),
 });
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  username: varchar("username", { length: 256 }),
+  email: varchar("email", { length: 256 }),
+  firstName: varchar("first_name", { length: 256 }),
+  lastName: varchar("last_name", { length: 256 }),
+  password: varchar("password", { length: 256 }),
+  phoneNumber: varchar("phone_number", { length: 15 }),
+  countryId: integer("country_id").references(() => countries.id),
+  cityId: integer("city_id").references(() => cities.id),
+  dateOfBirth: date("date_of_birth"),
+  genderId: integer("gender_id").references(() => gender.id),
+});
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
