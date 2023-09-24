ec9a4fab943feb0d16101e600b5fb5c6eeca9566
Beqa1G <beqa.gurgenidze.2013@gmail.com>
2023-09-24 20:59:59 +0400

    last commit

--- backend/src/utils/validatedotenv.ts
+++ backend/src/utils/validatedotenv.ts
@@ -4,5 +4,6 @@ import { port, str } from "envalid/dist/validators";
 export default cleanEnv(process.env, {
     DRIZZLE_DATABASE_URL: str(),
     PORT: port(),
+    ACCESS_TOKEN_SECRET: str({ desc: "Access token secret" }),
   });
 
