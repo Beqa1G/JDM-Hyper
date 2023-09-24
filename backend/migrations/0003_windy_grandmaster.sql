ae0e0229afd018392ac892f040bc13429fc247e2
Beqa1G <beqa.gurgenidze.2013@gmail.com>
2023-09-24 20:58:22 +0400

    aletered tabled

---
+++ backend/migrations/0003_windy_grandmaster.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users" ALTER COLUMN "gender_id" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_username_unique" UNIQUE("username");--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_email_unique" UNIQUE("email");
---
+++ backend/migrations/0004_keen_lifeguard.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "users" ADD CONSTRAINT "users_phone_number_unique" UNIQUE("phone_number");
