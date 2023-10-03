20eda142249ac65ae381b0c8ca13af8cbcd625a8
Beqa1G <beqa.gurgenidze.2013@gmail.com>
2023-10-03 12:55:47 +0400

    added page

---
+++ frontend/src/app/(register-login)/signuppage/SignupPageNavBar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./singuppage.module.css";
+import { usePathname} from "next/navigation";
+
+
+export function SignUpPageNavBar() {
+  const pathname = usePathname();
+
+
+  const isLoginPage = pathname === "/loginpage";
+  const linkText = isLoginPage ? "Register" : "Login";
+  const linkHref = isLoginPage ? "/signuppage" : "/loginpage";
+
+  return (
+    <header>
+      <div className={styles.NavWrapper}>
+        <div className={styles.logoWrapper}>
+          <Image src="/JDM.png" alt="company logo" width={25} height={25} />
+          <Link href={"/"} className={styles.colorBlack}>
+            JDM Hyper
+          </Link>
+        </div>
+        <nav>
+          <ul >
+            <li className={styles.listitem}  >
+            <Link className={styles.nodec} href={linkHref}>{linkText}</Link>
+             
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
